b021dd397f6a596a69411cdf2d4341a1444edd53
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-22 20:39:37 +0000

    commit all

--- index.php
+++ index.php
@@ -6,14 +6,12 @@
     <h1>Les classes, les attributs et les methodes</h1>
     <?php
 class Voiture {
-  private $couleur = "rouge";
-  private $vitesse = 0 ;
-  public function afficherCouleur () {
-    return $this -> couleur ;
-  }
-  public function repeindre($co) {
+  private $couleur;
+  private $vitesse;
+  public function __construct($co ="rouge",$vi = 0) {
     $this -> couleur = $co ;
-  }
+    $this -> vitesse = $vi ;
+  } // le construcor est appelé au moment de l'instanciation
   public function accelerer () {
     $this -> vitesse ++ ;
   }
@@ -21,16 +19,11 @@ class Voiture {
     return $this -> vitesse;
   }
 }
-$berline = new Voiture() ;
-$berline -> accelerer() ;
-$berline -> accelerer() ;
-$berline -> accelerer() ;
-$berline -> accelerer() ;
-$berline -> accelerer() ;
-$berline -> accelerer() ;
-
+$berline = new Voiture() ; // ici c'est l'instanciation.
   echo $berline -> afficherCompteur() ;
-// generalement les attributs il faut les déclarer autant que privé pour eviter que quelconque poura les modifier a l'interieur de la classe.
+// generalement les attributs il faut les déclarer autant que privé pour eviter que quelconque poura les modifier a l'interieur de la classe.a la non optimisation du code .
+
+// il est déconseillé d'instancier les attributs pendant la déclaration de la class car ca amène 
 ?>
 
   <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
